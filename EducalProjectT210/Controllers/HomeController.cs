ede36eab636303e423a1deb0ff09ee60f8d82e36
nsharifova <nurluay00@bk.ru>
2022-01-06 18:58:08 +0400

    Add project files.

---
+++ EducalProjectT210/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EducalProjectT210.Models;
+using EducalProjectT210.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EducalProjectT210.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CourseDBContext _context = new CourseDBContext();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeVm vm = new()
+            {
+                Categories = _context.Categories.ToList(),
+                ClassRooms = _context.ClassRooms.ToList(),
+            };
+            return View(_context.Categories.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EducalProjectT210/ViewModels/HomeVm.cs
@@ -0,0 +1,10 @@
+﻿using EducalProjectT210.Models;
+
+namespace EducalProjectT210.ViewModels
+{
+    public class HomeVm
+    {
+        public List<Category> Categories { get; set; }
+        public List<ClassRoom> ClassRooms { get; set; }
+    }
+}
